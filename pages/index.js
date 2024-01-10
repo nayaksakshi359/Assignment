9425f1837e783f0a168840bc946eee1512636221
sakshi <nayaksakshi34@gmail.com>
2024-01-10 23:08:56 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router"
+
+
+import Resume from "./design.js/Photo/Sakshi Nayak"
+
+export default function Home() {
+  const router = useRouter
+  return (
+    <div >
+    
+     
+     <Resume/>
+     
+    </div>
+  )
+}
